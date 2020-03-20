c917e6a46c46903f2c06d0b859d04c42f1ef2b03
Maksim Iashkov <Maksim_Iashkov@epam.com>
2020-03-20 23:29:44 +0300

    Вторая версия модуля 1

--- src/main/java/com/epam/maksim_iashkov/java/lesson1/task2/ApplicationStart.java
+++ src/main/java/com/epam/maksim_iashkov/java/lesson1/task2/ApplicationStart.java
@@ -1,11 +1,16 @@
 package com.epam.maksim_iashkov.java.lesson1.task2;
 
+/**
+ * Класс, реализующий входную точку для запуска программы
+ * @version 19.03.2020
+ * @author Maksim_Iashkov
+ */
 public class ApplicationStart {
 
+	/**Метод main - входная точка программы*/
     public static void main(String[] args) {
 	new StringProcessing().shortestAndLongest();
-	//new StringProcessing().moreThanAvg();
-	//new StringProcessing().lessThanAvg();
-	//new WordProcessing().differentSymbols();
+	new StringProcessing().moreThanAvg();
+	new StringProcessing().lessThanAvg();
     }
 }
--- src/main/java/com/epam/maksim_iashkov/java/lesson1/task3/CalculateIntNumber.java
+++ src/main/java/com/epam/maksim_iashkov/java/lesson1/task3/CalculateIntNumber.java
@@ -1,10 +1,21 @@
 package com.epam.maksim_iashkov.java.lesson1.task3;
 import java.util.Scanner;
 
+/**
+ * Класс, реализующий вычисление двух целых чисел
+ * @version 19.03.2020
+ * @author Maksim_Iashkov
+ */
 public class CalculateIntNumber {
-    Scanner scanner = new Scanner(System.in);
 
+    Scanner scanner = new Scanner(System.in); //Объявление переменной для сканера
+
+    /**
+     *Метод main - входная точка программы
+     */
     public static void main(String[] args) {
+
+        /*Инициализация глобальных переменных*/
         double num1 = 0.00;
         double num2 = 0.00;
         char operation = ' ';
@@ -18,38 +29,46 @@ public class CalculateIntNumber {
         operation = calculateIntNumber.getOperation();
         res = calculateIntNumber.generateCalculation(num1,num2,operation);
         System.out.println("Результат операции: "+res);
-        //System.out.println(num1);
-        //System.out.println(num2);
-        //System.out.println(operation);
     }
 
+    /**
+     *Метод считывания числа из консоли
+     */
     public double getNumber(){
         System.out.println("Введите целое число в диапазоне от -2147483648 до 2147483647:");
-        double num;
-        if(scanner.hasNextInt()) {
+        double num = 0.0;
+
+        if(scanner.hasNextInt()) {          //Проверка введенное числа на целочисленность
                 num = scanner.nextInt();
         } else {
             System.out.println("Вы допустили ошибку при вводе числа! Введите число заново");
             scanner.next();
-            num = getNumber();
+            num = getNumber();          //Если допущена ошибка при вводе числа - заново позволить ввести число
         }
         return num;
     }
 
+    /**
+     *Метод считывания операции из консоли
+     */
     public char getOperation(){
         System.out.println("Введите операцию (Одним символом: '+' для сложения, '-' для вычитания, '*' для умножения и '/' для деления):");
         char operation;
+
         scanner.hasNext();
             try {
-                operation = scanner.next(".").charAt(0);
+                operation = scanner.next(".").charAt(0);        //Считывание операции, которая должна уложиться в 1 символ
             } catch(Exception ex) {
                 System.out.println("Введенная операция должна вызываться одним символом! Введите операцию заново");
                 scanner.next();
-                operation = getOperation();
+                operation = getOperation();     //В случае срабатывания исключения - заново позволить ввести операцию
             }
         return operation;
     }
 
+    /**
+     *Метод реализации вычисления операции над двумя числами
+     */
     public double generateCalculation(double num1,double num2,char operation){
         double res;
         switch (operation) {
@@ -63,16 +82,16 @@ public class CalculateIntNumber {
                 res = num1*num2;
                 break;
             case '/':
-                if (num2 != 0){
+                if (num2 != 0){         //Проверка деления на нуль
                     res = num1/num2;
                 } else {
                     System.out.println("На ноль делить нельзя! Введите делитель заново");
-                    res = generateCalculation(num1, getNumber(), operation);
+                    res = generateCalculation(num1, getNumber(), operation);    //Если второе число при делении нуль - позволить заново ввести делитель
                 }
                 break;
             default:
                 System.out.println("Операция не распознана! Введите операцию заново");
-                res = generateCalculation(num1, num2, getOperation());
+                res = generateCalculation(num1, num2, getOperation());  //Если в блоке считывания операции был введен один неопознанный символ
         }
         return res;
     }
