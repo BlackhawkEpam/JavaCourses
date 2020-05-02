6b8140af2a25ee9538f5d0d9eb43a21f1ff3f29a
Nikolai Kobzev <Nikolai_Kobzev@epam.com>
2020-05-02 10:03:27 +0300

    CleanUp

--- src/main/java/com/epam/maksim_iashkov/java/lesson4/DynamicArray.java
+++ src/main/java/com/epam/maksim_iashkov/java/lesson4/DynamicArray.java
@@ -5,9 +5,13 @@ import java.util.Arrays;
 /**
  * Создание класса - динамического массива для 1 задачи
  */
+@SuppressWarnings("unchecked")
 public class DynamicArray<E> {
-    private int currentSize = 0;    //Текущий размер списка элементов
-    private Object[] dynArray;  //Объявление динамического массива
+
+    //Текущий размер списка элементов
+    private int currentSize = 0;
+    //Объявление динамического массива
+    private Object[] dynArray;
 
     /**
      * Конструктор массива, в который передается размер и сразу присваивается размеру массива
@@ -20,13 +24,19 @@ public class DynamicArray<E> {
      * Метод добавления элемента в конец списка динамического массива
      */
     public void add(E e) {
-        if (dynArray.length <= currentSize) {   //Если длина массива стала не больше размера списка - трансформировать массив
-            Object[] tmp = dynArray;    //Создать временный массив для хранения элементов
-            dynArray = new Object[currentSize * 2];     //Увеличить размер массива вдвое
-            System.arraycopy(tmp, 0, dynArray, 0, tmp.length);  //Переписать в новый увеличенный массив элементы перезаписанного из временного массива
+        //Если длина массива стала не больше размера списка - трансформировать массив
+        if (dynArray.length <= currentSize) {
+            //Создать временный массив для хранения элементов
+            Object[] tmp = dynArray;
+            //Увеличить размер массива вдвое
+            dynArray = new Object[currentSize * 2];
+            //Переписать в новый увеличенный массив элементы перезаписанного из временного массива
+            System.arraycopy(tmp, 0, dynArray, 0, tmp.length);
         }
-        dynArray[currentSize] = e;  //Присвоить последнему элементу списка введенное значение
-        currentSize++;  //Инкрементировать размер списка элементов
+        //Присвоить последнему элементу списка введенное значение
+        dynArray[currentSize] = e;
+        //Инкрементировать размер списка элементов
+        currentSize++;
     }
 
     /**
@@ -34,34 +44,43 @@ public class DynamicArray<E> {
      */
     public void remove(int i) {
         try {
-            if ((i >= currentSize) || (i < 0))
+            if ((i >= currentSize) || (i < 0)) {
                 throw new IndexOutOfBoundsException("Элемент с данным индексом отсутствует в массиве!");
-            System.arraycopy(dynArray, i + 1, dynArray, i, dynArray.length - 1 - i);    //Перезаписать всю последовательность массива с i+1 на места, начиная с i-того
-            currentSize--;  //Декрементировать размер списка элементов
+            }
+            // Перезаписать всю последовательность массива с i+1 на места, начиная с i-того
+            System.arraycopy(dynArray, i + 1, dynArray, i, dynArray.length - 1 - i);
+            //Декрементировать размер списка элементов
+            currentSize--;
         } catch (IndexOutOfBoundsException ex) {
-            System.out.println(ex.getMessage());    //Отловить исключение - если введен отрицательный или несуществующий индекс
+            //Отловить исключение - если введен отрицательный или несуществующий индекс
+            System.out.println(ex.getMessage());
         }
     }
 
     /**
      * Метод получения элемента по индексу
      */
-    @SuppressWarnings("unchecked")
     public E get(int i) {
         try {
-            if ((i >= currentSize) || (i < 0))
+            if ((i >= currentSize) || (i < 0)) {
                 throw new IndexOutOfBoundsException("Элемент с данным индексом отсутствует в массиве! Возвращен последний элемент массива");
+            }
         } catch (IndexOutOfBoundsException exc) {
-            System.out.println(exc.getMessage());   //Отловить исключение - если введенный индекс не существует или отрицательный
-            i = currentSize - 1;    //В случае исключения - вывести последний элемент
+            //Отловить исключение - если введенный индекс не существует или отрицательный
+            System.out.println(exc.getMessage());
+            //В случае исключения - вывести последний элемент
+            i = currentSize - 1;
         }
-        return (E) dynArray[i]; //Вернуть элемент по индексу
+        //Вернуть элемент по индексу
+        return (E) dynArray[i];
     }
 
     /**
      * Метод печати внутреннего состояния массива в консоль
      */
-    public void tostring() {
-        System.out.println("Внутреннее состояние массива: " + Arrays.toString(dynArray));
+    public String toString() {
+        String result = Arrays.toString(dynArray);
+        System.out.println("Внутреннее состояние массива: " + result);
+        return result;
     }
 }
